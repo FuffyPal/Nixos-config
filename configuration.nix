cbacaea35f52a76bc71934ca0faa21aa4fd6b73d
FluffyPal <126797355+FuffyPal@users.noreply.github.com>
2025-09-02 21:02:51 +0000

    Update configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -8,12 +8,11 @@
   imports =
     [ # Include the results of the hardware scan.
       ./hardware-configuration.nix
-      ./gnome.nix
-      ./vim.nix
-      ./networking.nix
-#      ./Flaumig.nix
       ./nvidia.nix
-#      ./image.nix
+      ./networking.nix
+      ./gnome.nix
+      ./packages.nix
+      ./flatpak.nix
 
     ];
 
@@ -48,9 +47,6 @@
   # Configure console keymap
   console.keyMap = "trq";
 
-  # Enable CUPS to print documents.
-  services.printing.enable = false;
-
   # Enable sound with pipewire.
   services.pulseaudio.enable = false;
   security.rtkit.enable = true;
@@ -68,79 +64,34 @@
   };
 
   # Enable touchpad support (enabled default in most desktopManager).
-  services.libinput.enable = true;
+  # services.xserver.libinput.enable = true;
 
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.[redected] = {
     isNormalUser = true;
-    description = "FluffyPal";
+    description = "[redected]";
     extraGroups = [ "networkmanager" "wheel" ];
     packages = with pkgs; [
+    #  thunderbird
     ];
   };
 
   # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
-  
-  # Allow flatpak service
-  services.flatpak.enable = true;
-
-  # vim 
-  programs.vim.enable = true;
 
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
-    vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+  #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
   #  wget
-    flatpak
-    podman
-    zapret
-    protonvpn-gui
   ];
 
-  # fstrim settings
-  services.fstrim = {
-    enable = true;
-    interval = "d";
-  };
-
-  # Podman settings
-  virtualisation.podman  = {
-    dockerSocket.enable = false;
-  #  enableNvidia = true;
-    enable = true;
-    autoPrune.enable = false;
-  };
-
-  # Systemd service settings
-  systemd.services = {
-    NetworkManager-wait-online = {
-      enable = false;
-    };
-  };
-  
-  # Kernel Settings
-  boot.kernelPackages = pkgs.linuxPackages;
-
-  # System auto upgrade settings
-  system.autoUpgrade = {
-    enable = true;
-    dates = "Mon,Tue,Wed,Thu,Fri,Sat,Sun 03:00:00"; # Her gün sabah 3'te güncelleme
-    allowReboot = true; # Güncelleme sonrası otomatik yeniden başlatma
-    channel = "https://nixos.org/channels/nixos-25.05";
-    randomizedDelaySec = "30m"; # Gecikmeyi 30 dakikaya düşürme
-  };
-  # bash shell settings
-  programs.bash = {
-    # Alias
-    shellAliases = {
-      panic="sudo systemctl stop NetworkManager.service";
-      develenv="podman run -it --rm --name env -p 2222:22 -v $HOME/Projects:/workspace localhost/fluffy:env";
-      up="sudo nix-channel --update && sudo nixos-rebuild switch --upgrade-all && flatpak update -y";
-    };
-  }; 
-
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
   system.stateVersion = "25.05"; # Did you read the comment?
 
 }
